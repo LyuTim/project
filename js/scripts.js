754d00491cd8c6679d40889eb2ebcf98c894f4ba
Lyubov <timokhova.lyu@yandex.ru>
2021-07-13 01:35:05 +0300

    proekt_10

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-/* slider =================================================================================*/
+/* slider=====================*/
 $(function(){
     sliderRun(); // запускаем нашу программу, когда готов DOM
 });
@@ -78,98 +78,24 @@ function sliderRun() {
 
 
 
-
-
-
-
-
-
-
-
-
-/* =================================================================================*/
-
-$(function(){
-    $('.submenu_mover').click(function(){
-        if ($(this).parent().hasClass('open')) {
-            $('.catmenu_item.open').removeClass('open').find('.submenu').animate({
-                height: 0
-            }, 1000);
-        } else {
-            $('.catmenu_item.open').removeClass('open').find('.submenu').animate({
-                height: 0
-            }, 1000);
-            $(this).parent().addClass('open').find('.submenu').animate({
-                height: ($(this).parent().find('.submenu a').length * 24)
-            }, 1000);
-        }
-    });
-    
-    $('button.basket').click(function(){
-        $('.popup-desk').addClass('active');
-        $('.popup').html('<p class="popup-header">Личный кабинет</p><input type="text" name="fullname" placeholder="Логин"><input type="password" name="password" placeholder="Пароль"><button type="submit">Войти</button><a href="https://yandex.ru/" class="register">Зарегистрироваться</a>');
-        $('.basket').html(6);
-    });
-    
-    $('.popup-desk').click(function(e){
-        if (e.target == this) {
-            $(this).removeClass('active');
-            $('.popup').empty();
-            $('.basket').html(5);
-        }
-    });
-    
-    $(document).on('click', '.register', function(e){
-        e.preventDefault();
-        if ($('.basket').html()==6) {
-            $('.popup').html('<p class="popup-header">Личный кабинет закрыт на ремонт.<br>Регистрации не будет до 1 января.</p><a href="https://yandex.ru/" class="register">Войти</a>');
-            $('.basket').html(5);
-        } else {
-            $('.popup').html('<p class="popup-header">Личный кабинет</p><input type="text" name="fullname" placeholder="Логин"><input type="password" name="password" placeholder="Пароль"><button type="submit">Войти</button><a href="https://yandex.ru/" class="register">Зарегистрироваться</a>');
-            $('.basket').html(6);
-        }
-    });
-    
-    $(document).on('click', 'button[type="submit"]', function(){
-        alert('OGOGO!!!');
-    });
-    
-    $(document).on('click', '.order .del > div', function(){
-        tovarDelete(this);
-    });
-    
-    $(document).on('change input', '.order .num > input', function(){
-        tovarChange(this);
-    });
-	$('#privet').click(function(){alert('Привет!')});
-});
-
-
-
 /* order */
 const order = [
     {
         id: 5711,
-        name: 'Товар 1',
-        value: 10,
-        price: 5
+        value: 10
     },
     {
         id: 3432,
-        name: 'Товар 2',
-        value: 10,
-        price: 10
+        value: 10
     },
     {
         id: 4846,
-        name: 'Товар 3',
-        value: 10,
-        price: 15
+        value: 10
     }
-]
+];
 function tovarDelete(point) {
-    let b = point.parentNode.parentNode;
-    let t_id = b.querySelector('th').dataset.tovar;
+    let b = point.parentNode.parentNode; // нашли строку, в которой лежит наш товар
+    let t_id = b.querySelector('th').dataset.tovar; // находим id товара
     for (let i = 0; i < order.length; i++) {
         if (order[i].id == t_id) {
             order.splice(i, 1);
@@ -181,28 +107,296 @@ function tovarDelete(point) {
     if (order.length > 0) {
         tovarCount();
     } else {
-        document.querySelector('.table').remove();
-        document.querySelector('.nihil').style = 'display:block';
+        orderEmpty();
     }
 }
-function tovarChange(a) {
-    let b = a.parentNode.parentNode;
-	let newVal = b.querySelector('input').value();
-	let t_id = b.querySelector('th').dataset.tovar;
-    for (let i = 0; i < order.length; i++) {
-        if (order[i].id == t_id) {
-            order[i].value = order[1].value(newVal);
-			console.log(newVal);
+function tovarChange(point) {
+    let new_quantity = point.value;
+    if (new_quantity <= 0) {
+        tovarDelete(point);
+    } else {
+        let tovar_id = point.parentNode.parentNode.querySelector('th').dataset.tovar;
+        for (let i = 0; i < order.length; i++) {
+            if (order[i].id == tovar_id) {
+                order[i].value = new_quantity;
+                console.log(order[i]);
+                break;
+            }
+        }
+        tovarCount();
+    }
+}
+function tovarCount() { // пересчет товара
+    let itog = 0; // общий итог
+    for (let i = 0; i < order.length; i++) { // перебираем в цикле корзину
+        const row = $('.order .table tbody tr').eq(i); // берем строку, соответствующую по порядковому номеру перебираемому товару в корзине
+        row.find('th').html(i + 1); // выставляем порядковый номер товара. +1 потому что надо считать с 1, а у нас счет с 0
+        row.find('.sum').html(row.find('.rub').html() * order[i].value); // в ячейку с классом sum кладем произведение количества товара, взятого из корзины, на цену товара, взятую из ячейки с классом rub
+        itog += +row.find('.sum').html(); // плюсуем к итогу содержимое ячейки с классом sum (произведение количества на цену нашего товара)
+    }
+    $('.order .table .allsum').html(itog); // кладем итог в ячейку с классом allsum
+}
+function orderEmpty() {
+    $('.order').addClass('empty');
+}
+function orderAction() {
+    let data = {}; // создаем переменную, чтобы собрать все данные о заказе для отправки
+    data.order = order; // кладем туда корзину
+    data.customer = {}; // кладем данные о заказчике
+    data.customer.name = $('#name').val();
+    data.customer.address = $('#address').val();
+    data.customer.email = $('#email').val();
+    data.date = $('#date').val(); // кладем желаемую дату отгрузки
+    data.comment = $('#comment').val(); // кладем комментарий заказчика
+    let err = checkData(data);
+    if (err) { // проверяем данные на отсутствие ошибок
+        showErrors(err); // если ошибки есть, показываем их и прекращаем нашу функцию.
+        console.log('errors!'); // сообщаем в консоли, что ошибки есть.
+        return;
+    } // здесь мы поленились писать else
+    /* для реального магазина здесь следовало бы написать вот так:
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: data,
+        success: success
+    });
+    */
+    console.log(data);
+}
+function checkData(data) {
+    let arr = []; // ошибок может быть разное количество, лучше собрать их в массив
+    for (let item in data.order) { // проверяем товары в корзине
+        if (item.value <= 0) {
+            arr.push(['value', item.id]); // если товара 0 или меньше, отмечаем ошибку
         }
     }
-	
-	// опознать товар
-    // найти его в ордере
-    // изменить количество в ордере
-    // вызвать пересчет +
-    
-    tovarCount();
+    for (let key of ['name', 'address', 'email']) { // перебираем сведения о заказчике
+        if (data.customer[key].length == 0) {
+            arr.push(['empty', key]); // если поле не заполнено, отмечаем ошибку
+        }
+    }
+    if (data.date.length == 0) { // проверяем дату
+        arr.push(['empty', 'date']); // если поле не заполнено, отмечаем ошибку
+    } else {
+        let a = makeSelectedDate(data.date); // создаем дату из записи в поле date, время суток 0:00:00.000
+        let b = new Date(); // создаем дату сегодня, сейчас
+        b = new Date(b.getFullYear(), b.getMonth(), b.getDate()); // создаем дату сегодня в 0:00:00.000
+        if ((a - b) != (1000 * 60 * 60 * 24 * 2)) {
+            arr.push(['error', 'date']); // если поле заполнено, но дата не соответствует "через день", отмечаем ошибку
+        }
+    }
+    if (arr.length > 0) {
+        return arr; // если ошибки есть, возращаем массив ошибок
+    } else {
+        return false; // иначе возвращаем false
+    }
 }
-function tovarCount() {
-    alert('It works!');
+function showErrors(err) {
+    for (let unit of err) { // перебираем массив ошибок
+        if (typeof unit[1] == 'number') { // если указатель на поле - число, речь идет о товаре
+            for (let i = 0; i < order.length; i++) {
+                if (order[i].id == unit[1]) { // перебираем товары, находим нужный id, помечаем нужную строку таблицы
+                    $('tbody .tr').eq(i).addClass('error').prop('data-error', unit[0]);
+                }
+            }
+        } else { // используем id поля с ошибкой в селекторе jquery, помечаем нужное поле
+            $('#' + unit[1]).addClass('error').prop('data-error', unit[0]);
+        }
+    }
+}
+
+
+$(function(){
+/*
+1. В черхней черной полосе в шапке на любой странице проекта (ориентируемся на index) есть блок с текстом "Ваш город: Москва". Прикрепляем к нему обработчик кликов.
+2. Обработчик поднимает попап (или вызывает confirm) с вопросом "Ваш город - Москва?".
+3. Варианты ответа - true, false. В попапе их присылают обработчики на кнопках попапа, сам попап при этом закрывается. В конфирме их возвращает конфирм.
+4. При ответе false в блоке пишем "Ваш город: Немосква".
+5. При ответе true ничего больше не делаем.
+*/
+    /*
+    // берем элемент с нужным id поиском по документу, навешиваем на него слушатель события "клик"
+    document.getElementById('yourcity').addEventListener('click', function(){
+        // делаем видимым блок - держатель попапа (при помощи класса active, свойства для которого у нас уже заранее прописаны). при этом мы получаем готовый скринер, уже обрабатывающий клик мимо попапа.
+        document.querySelector('.popup-desk').classList.add('active');
+        // кладем в держатель попапа наш попап. при этом используем заранее прописанные стили.
+        document.querySelector('.popup-desk').innerHTML = '<div class="popup gorod"><p>Ваш город - Москва?</p><button type="button">ДА</button><button type="button">НЕТ</button></div>';
+        // выбираем кнопки (в виде массива) и вешаем на них обработчик событий
+        document.querySelectorAll('.gorod button').forEach(item => item.addEventListener('click', clickGorod));
+
+        // выносим функцию-обработчик в отдельную функцию, чтобы было удобнее ее навешивать и код лучше читался
+        function clickGorod(event) {
+            if (event.target.innerHTML == 'НЕТ') {
+                document.getElementById('yourcity').innerHTML = '<p>Ваш город: Немосква</p>';
+            } else {
+                document.getElementById('yourcity').innerHTML = '<p>Ваш город: Москва</p>';
+            }
+            document.querySelector('.popup-desk').dispatchEvent(new Event("click"));
+        }
+    });
+    */
+    $('#yourcity').click(function(){
+        $('.popup-desk').addClass('active').html('<div class="popup gorod"><p>Ваш город - Москва?</p><button class="yes" type="button">ДА</button><button class="no"  type="button">НЕТ</button></div>');
+        $('.gorod button').click(function(){
+            // if $('.gorod button').index(this) == 1
+            // if $(this).html() == 'НЕТ'
+            if ($(this).hasClass('no')) {
+                $('#yourcity').html('<p>Ваш город: Немосква</p>');
+            } else {
+                $('#yourcity').html('<p>Ваш город: Москва</p>');
+            }
+            $('.popup-desk').click();
+        })
+    });
+    
+    
+    /*
+    document.getElementById('yourcity').addEventListener('click', function(){
+        (confirm('Ваш город - Москва?')) ? (document.getElementById('yourcity').innerHTML = '<p>Ваш город: Москва</p>') : (document.getElementById('yourcity').innerHTML = '<p>Ваш город: Немосква</p>');
+    });
+    */
+   
+    
+    
+    
+    $('.submenu_mover').click(function(){
+        if ($(this).parent().hasClass('open')) {
+            $('.catmenu_item.open').removeClass('open').find('.submenu').animate({
+                height: 0
+            }, 1000);
+        } else {
+            $('.catmenu_item.open').removeClass('open').find('.submenu').animate({
+                height: 0
+            }, 1000);
+            $(this).parent().addClass('open').find('.submenu').animate({
+                height: ($(this).parent().find('.submenu a').length * $('.submenu a').height())
+            }, 1000);
+        }
+    });
+    
+    $('.login').click(function(){
+        $('.popup-desk').addClass('active');
+        $('.popup-desk').html('<div class="popup log"></div>');
+        $('.popup').html('<p class="popup-header">Личный кабинет</p><input type="text" name="fullname" placeholder="Логин"><input type="password" name="password" placeholder="Пароль"><button type="submit">Войти</button><a href="https://yandex.ru/" class="register">Зарегистрироваться</a>');
+    });
+    
+    $('.popup-desk').click(function(e){
+        if (e.target == this) {
+            $(this).removeClass('active');
+            $('.popup-desk').empty();
+        }
+    });
+    
+    $(document).on('click', '.register', function(e){
+        e.preventDefault();
+        if ($('.popup').hasClass('log')) {
+            $('.popup').html('<p class="popup-header">Личный кабинет закрыт на ремонт.<br>Регистрации не будет до 1 января.</p><a href="https://yandex.ru/" class="register">Войти</a>');
+            $('.popup').removeClass('log');
+        } else {
+            $('.popup').html('<p class="popup-header">Личный кабинет</p><input type="text" name="fullname" placeholder="Логин"><input type="password" name="password" placeholder="Пароль"><button type="submit">Войти</button><a href="https://yandex.ru/" class="register">Зарегистрироваться</a>');
+            $('.popup').addClass('log');
+        }
+    });
+    
+    $(document).on('click', '.order .del > div', function(){
+        tovarDelete(this);
+    });
+    
+    $(document).on('input', '.order .num > input', function(){
+        tovarChange(this);
+    });
+    
+    if ($('#date').length) {
+        $('#date').click(function(){
+            if ($('#date').val()) {
+                selected_day = makeSelectedDate($('#date').val());
+                makePopup(selected_day.getFullYear(),selected_day.getMonth());
+            } else {
+                makePopup(TODAY.getFullYear(),TODAY.getMonth());
+            }
+        });
+    
+        $('#date').mask('00-00-0000');
+    }
+    
+    
+    $('#orderdata').on('submit', function(e){// отправка формы
+        e.preventDefault();
+        orderAction();
+    })
+    
+    if ($('.product').length) {
+        $('.main-image').on('click', 'img', seebigimage);
+        $('.small-image').on('click', 'img', changeimage);
+    }
+    
+    if ($('.small-image > img').length) {
+        $('.small-image > img').each(function(){
+            $(this).css({
+                'margin-top': (160 - $(this).height()) / 2,
+                'margin-left': (160 - $(this).width()) / 2
+            })
+        });
+    }
+});
+
+$(document).ready(function() {
+  $(".getting").change(function() {
+
+    if ($('#pickup').prop("checked")) {
+      $('#select').fadeIn();
+	  $('#deliv').fadeOut();
+    } else if ($('#delivery').prop("checked")) {
+		$('#deliv').fadeIn();
+		$('#select').fadeOut();
+	}
+	});
+});
+
+
+/* gallery =================================================================*/
+function seebigimage() {
+    // получаем адрес большой картинки
+    let imageurl = document.querySelector('.main-image img').src.split('_medium.jpg').join('_big.jpg');
+    
+    // вставляем в попап верстку и адрес для картинки, а также кнопку-крестик для уборки попапа
+    document.querySelector('.popup-desk').innerHTML = '<div class="popup lightbox"><img src="' + imageurl + '"><div class="cross">+</div></div>';
+    
+    // получаем доступные размеры экрана и соотношение сторон картинки
+    // -120 пикселей - потому что у нас по стилям отступы от края экрана до попапа по 30 пикселей и поля попапа тоже по 30 пикселей с каждой стороны
+    let winsize = {
+        w: document.documentElement.clientWidth - 120,
+        h: document.documentElement.clientHeight - 120
+    };
+    let sides = document.querySelector('.main-image img').clientWidth / document.querySelector('.main-image img').clientHeight;
+    
+    // сравниваем реальные размеры попапа и размеры при правильном соотношении сторон у картинки. при необходимости вносим исправления
+    if (winsize.w / sides > winsize.h) {
+        let correction = (winsize.w - (winsize.h * sides)) / 2;
+        document.querySelector('.popup').style = 'margin: 0 ' + correction + 'px';
+        winsize.w = winsize.h * sides;
+    } else {
+        winsize.h = winsize.w / sides;
+    }
+    
+    // прописываем размеры картинки
+    document.querySelector('.lightbox img').style = 'width:' + winsize.w + 'px;height:' + winsize.h + 'px';
+    
+    // прописываем уборку попапа по клику на кнопку-крестик
+    document.querySelector('.cross').addEventListener('click',function(){
+        document.querySelector('.popup-desk').dispatchEvent(new Event("click"));
+    });
+    
+    // проявляем попап
+    document.querySelector('.popup-desk').classList.add('active');
+}
+function changeimage(event) { // аргументом будет событие клика
+    // из объекта события мы берем указатель на место события - event.target
+    // это место - наша картинка
+    // из ее атрибута src вычисляем адрес картинки среднего размера
+    let imageurl = event.target.src.split('_small').join('_medium');
+    
+    // всталяем этот адрес в атрибут src главной картинки
+    document.querySelector('.main-image img').src = imageurl;
 }
